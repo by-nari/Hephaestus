9df484cc68a2c1f0bb9c158ea62398ff2f80a8ee
Vir <mrvir@users.noreply.github.com>
2017-07-10 18:33:00 +0700

    add lossless support for Nhaccuatui

--- server.py
+++ server.py
@@ -19,39 +19,68 @@ def hello():
     form = WebForm(request.form)
  
     if request.method == 'POST':
+
         link=request.form['link']
+        mp3_valid = re.match("(https?:\/\/)?mp3\.zing\.vn\/bai-hat\/[\w\d\-]+/([\w\d]{8})\.html", link)
+        nct_valid = re.match("https?:\/\/www\.nhaccuatui\.com\/bai-hat\/[-.a-z0-9A-Z]+\.html", link)
  
         if form.validate():
-            link128 = Process(link)
-            flash(link128)
+
+            if mp3_valid:
+                link128 =MP3(link)
+                flash(link128)
+
+            elif nct_valid:
+                
+                link128, link320, lossless = NCT(link)
+                flash(link128)
+
+                if "hq.mp3" in link320:
+                    flash(link320)
+
+                if ".flac" in lossless:
+                    flash(lossless)
+
+            else:
+                flash("Link bạn vừa nhập vào không chính xác, vui lòng kiểm tra lại")
+
         else:
             flash('Lỗi: Bạn cần nhập link bài hát vào.')
  
     return render_template('main.html', form=form)
 
-def Process(link):
-    mp3_valid = re.match("(https?:\/\/)?mp3\.zing\.vn\/bai-hat\/[\w\d\-]+/([\w\d]{8})\.html", link)
-    nct_valid = re.match("https?:\/\/www\.nhaccuatui\.com\/bai-hat\/[-.a-z0-9A-Z]+\.html", link)
+def MP3(link):
 
-    if mp3_valid:
-        string_valid = re.search("([A-Z0-9]{8})", link).group(1)
-        content = requests.get('http://phulieuminhkhang.com/images/Sanpham/api.php?parameter='+string_valid).text
-        decoded = json.loads(content)
-        link128 = decoded['link_download']['128']
-        link320 = decoded['source']['320']
-        return link128
-    elif nct_valid:
-        content = requests.get(link).text
-        xml = re.search("https?:\/\/www\.nhaccuatui\.com\/flash\/xml\?key1=[0-9a-z]{30,40}", content).group(0)
-        headers = {'content-type': 'application/xml'}
-        xmlcontent = requests.get(xml, headers=headers).text
-        link128 = re.search("https?:\/\/[^\/]+\.nixcdn\.com\/[-_\/a-zA-Z0-9]+\.mp3", xmlcontent).group(0)
-        link320 = "Empty"
-        return link128
-    else:
-        result = "Link bạn vừa nhập vào không chính xác, vui lòng kiểm tra lại"
-        return result
+    string_valid = re.search("([A-Z0-9]{8})", link).group(1)
+    content = requests.get('http://phulieuminhkhang.com/images/Sanpham/api.php?parameter='+string_valid).text
+    decoded = json.loads(content)
+    link128 = decoded['link_download']['128']
+    link320 = decoded['source']['320']
+    return link128
 
+def NCT(link):
+    
+    id = link.split('.')[3]
+    url = 'https://graph.nhaccuatui.com/v1/commons/token'
+    headers = {'Content-Type': 'application/x-www-form-urlencoded', 'Host': 'graph.nhaccuatui.com', 'Connection': 'Keep-Alive'}
+
+    payload = {'deviceinfo': '{"DeviceID":"dd03852ada21ec149103d02f76eb0a04","DeviceName":"AppTroLyBeDieu", \
+"OsName":"WINDOWS","OsVersion":"8.0","AppName":"NCTTablet","AppTroLyBeDieu":"1.3.0", \
+"UserName":"0","QualityPlay":"128","QualityDownload":"128","QualityCloud":"128","Network":"WIFI","Provider":"NCTCorp"}', \
+'md5': 'ebd547335f855f3e4f7136f92ccc6955', 'timestamp': '1499177482892'}
+
+    r = requests.post(url, data=payload)
+    decoded = json.loads(r.text)
+    token = decoded['data']['accessToken']
+
+    gurl = 'https://graph.nhaccuatui.com/v1/songs/'+id+'?access_token='+token
+    content = requests.get(gurl)
+    result = json.loads(content.text)
+    link128 = result['data']['11']
+    link320 = result['data']['12']
+    lossless = result['data']['19']
+
+    return link128, link320, lossless
 
 
 if __name__ == "__main__":
